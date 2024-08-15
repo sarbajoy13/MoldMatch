13c65442e5985ee4245a9693856bce4cca1f69c8
sarbajoy13 <sarbajoy13chatterjee@gmail.com>
2024-08-15 20:52:10 +0530

    "S:\Carousel1.mp4"

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+// JavaScript for Carousel
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.carousel-content');
+    const totalSlides = slides.children.length;
+    
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Placeholder: Automatically advance slides (optional)
+setInterval(nextSlide, 5000);
